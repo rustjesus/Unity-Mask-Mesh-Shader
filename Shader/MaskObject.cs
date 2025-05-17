6720149aa06fe14b37b04750f64c317e1e022266
Eric Young <157120247+rustjesus@users.noreply.github.com>
2025-05-17 12:51:05 -0500

    Add files via upload

---
+++ Shader/MaskObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaskObject : MonoBehaviour
+{
+    [Header("put on mask obj with mask material and put obj to mask in objmasked")]
+    public GameObject[] ObjMasked;
+    void Start()
+    {
+        for (int i = 0; i < ObjMasked.Length; i++)
+        {
+            ObjMasked[i].GetComponent<MeshRenderer>().material.renderQueue = 3002;
+        }
+        Destroy(this);
+    }
+
+}
+
